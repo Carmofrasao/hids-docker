d98559d4d7dc366b067237581745e08815197d25
Anderson <anderfrasao52@gmail.com>
2024-02-22 18:27:50 -0300

    Add scripts

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
 # HIDS Docker
 
+## How do I get set up? ##
 
+The following components should be installed:
+
+```
+* python 3.8.5
+* pip3 20.0.2
+  * scikit-learn 0.22.2
+```
+
+### Install ###
+
+Python3 and pip3 are required for testing. Arch linux installation guide:
+```
+sudo pacman -S python python-pip
+```
+
+Debian installation guide:
+```
+sudo apt install python3 python3-pip
+```
+
+### Setup ###
+
+Clone this repository:
+```
+git clone https://github.com/gabrielruschel/hids-docker
+```
+
+And install the dependencies using pip3:
+```
+cd hids-docker/
+pip3 install -r requirements.txt
+```
+
+## Examples ##
+
+To run the tests, just use the following command:
+```
+python3 main.py [-d {sbseg,iscc}] [-f {raw,filter}] <window_size>
+python3 -u TCC/scripts/rf.py --dataset histograma.csv
+```
+* `-d`: specifies which dataset to use (default: iscc)
+* `-f`: specifies which filter mode to use (default: raw)
+* `window_size`: specifies the size of the window be used in the tests.
+
+The `-h` argument shows the help message. It is possible to edit in the main section of the code which specific methods you want to test.
---
+++ histogram.py
@@ -0,0 +1,57 @@
+import os
+import syscall from syscalls; 
+
+print("id,t", end=',')
+i = 0
+for sys in syscall:
+   if i == 362:
+       print(sys)
+   else:
+       print(sys, end=',')
+   i+=1
+i=0
+id = 0
+for _, _, arquivos in os.walk('./wordpress/exec/'):
+    for arquivo in arquivos:
+        with open(f'./wordpress/exec/{arquivo}', "r") as f:
+            lines = f.readlines()
+
+        # Trecho para contar o numero de chamadas de cada syscall
+        for line in lines:
+            linhaAux = line.split(' ')
+            call = linhaAux[0]
+            syscall[call] += 1
+        print(f'{id},0', end=',')
+        id += 1
+        for sys in syscall.values():
+            if i == 362:
+                print(sys/2)
+            else:
+                print(sys/2, end=',')
+            i+=1
+        i=0
+        for sys in syscall:
+            syscall[sys] = 0
+        
+for _, _, arquivos in os.walk('./wordpress/normal/'):
+    for arquivo in arquivos:
+        with open(f'./wordpress/normal/{arquivo}', "r") as f:
+            lines = f.readlines()
+
+        # Trecho para contar o numero de chamadas de cada syscall
+        for line in lines:
+            linhaAux = line.split(' ')
+            call = linhaAux[0]
+            syscall[call] += 1
+        print(f'{id},1', end=',')
+        id += 1
+        for sys in syscall.values():
+            if i == 362:
+                print(sys/2)
+            else:
+                print(sys/2, end=',')
+            i+=1
+        i=0
+                
+        for sys in syscall:
+            syscall[sys] = 0
